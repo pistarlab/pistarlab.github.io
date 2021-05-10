25c799fb5ed91b1961c6a6724ca4adb833573e56
Brandyn <bkusenda@gmail.com>
2021-05-10 12:24:13 -0700

    adding dist directory

---
+++ dist/precache-manifest.edb48780d1def06d67aee509ba8d5525.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a1dbc1137e71d1dbfb8f",
+    "url": "/css/app.037d6706.css"
+  },
+  {
+    "revision": "a1d8cfc2667b52c6ae24",
+    "url": "/css/chunk-vendors.5161535d.css"
+  },
+  {
+    "revision": "023157ea9f4f150b85e32a6f357d7b4a",
+    "url": "/img/pistar_edit_w.023157ea.png"
+  },
+  {
+    "revision": "27f2186b3161d9514a5d7dd263c4a475",
+    "url": "/img/pistar_logo_black.27f2186b.png"
+  },
+  {
+    "revision": "eaf2f6f019c6bf28525dc42e35e7d9f2",
+    "url": "/img/pistarlab_agent_home.eaf2f6f0.png"
+  },
+  {
+    "revision": "1ab16db1c5500767e3c2b82275e41e7c",
+    "url": "/img/pistarlab_envs.1ab16db1.png"
+  },
+  {
+    "revision": "22260d58cdb757067dca0870c8d064cd",
+    "url": "/img/pistarlab_session_stats.22260d58.png"
+  },
+  {
+    "revision": "b50741c8d55291562f48b6dccc156e9e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d97a643dade90aafdcfe",
+    "url": "/js/about.bbe39317.js"
+  },
+  {
+    "revision": "a1dbc1137e71d1dbfb8f",
+    "url": "/js/app.328aa2cc.js"
+  },
+  {
+    "revision": "a1d8cfc2667b52c6ae24",
+    "url": "/js/chunk-vendors.069d8e08.js"
+  },
+  {
+    "revision": "9f9fa474f6b63651b5e447b7d220570e",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "69b5f5a8768918799104f8f1125132b4",
+    "url": "/pistar.ico"
+  },
+  {
+    "revision": "e13b6e2c1b5807d9b26eb3da67fe10ca",
+    "url": "/splash.jpg"
+  }
+]);
